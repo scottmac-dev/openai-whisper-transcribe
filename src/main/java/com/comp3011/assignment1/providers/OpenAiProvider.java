f8f833e43bd5ded6247694fd1f1ff398d4d1880b
scottmac-uni <a1745541@adelaide.edu.au>
2026-08-20 00:22:24 +0930

    Build out the provider transcribe function to OpenAI endpoint

--- src/main/java/com/comp3011/assignment1/providers/OpenAiProvider.java
+++ src/main/java/com/comp3011/assignment1/providers/OpenAiProvider.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,16 +35,45 @@ public class OpenAiProvider {
 
     // Extract API key on instantiation
     public OpenAiProvider(@Value("${openai.api-key}") String apiKey) {
+    	
+    	// Create REST client using whisper API endpoint + api key in auth header
         this.restClient = RestClient.builder()
                 .baseUrl(WHISPER_BASE_URL)
                 .defaultHeader("Authorization", "Bearer " + apiKey)
                 .build();
     }
     
-    // TODO
+    /*
+     * Uploads file to OpenAI whisper endpoint
+     * 
+     * Expected request format:
+     * 	- Authorization: Bearer $API_KEY
+     * 	- Content-Type: multipart/form-data
+     * 	- file: /path/to/audio.webm
+     * 	- model: whisper-1
+     * 	- response_format: verbose_json
+     * 
+     * Accepted audio files: mp3, mp4, mpeg, mpga, wav, webm, m4a
+     * Browser recording does webm using MediaRecorder 
+     * 
+     * Available reponse formats: text, json, verbose_json, srt, vtt
+     * verbose_json will give the most meta data to work with
+     *  */
     public OpenAiResponse transcribe(MultipartFile audio) throws IOException {
-    	throw new IOException("TODO");
     	
+    	// Build API request to whisper endpoint
+        MultipartBodyBuilder body = new MultipartBodyBuilder();
+        body.part("file", audio.getResource())
+            .filename(audio.getOriginalFilename());
+        body.part("model", MODEL);
+        body.part("response_format", "verbose_json");
+        
+        // POST to endpoint, extract return type into response class
+        return restClient.post()
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(body.build())
+                .retrieve()
+                .body(OpenAiResponse.class);
     }
 
 
