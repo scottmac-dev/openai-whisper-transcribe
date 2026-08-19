835bfdc948b815fbfa6b4ec9ab7149fcdd1fa5e6
scottmac-uni <a1745541@adelaide.edu.au>
2026-08-19 22:47:46 +0930

    Init bootstrap springboot project

---
+++ src/main/java/com/comp3011/assignment1/Assignment1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.comp3011.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp3011/assignment1/Assignment1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp3011.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
